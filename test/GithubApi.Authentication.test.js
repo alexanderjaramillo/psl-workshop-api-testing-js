a2799ebfab68fd5459ce49a077c41af776be6474
alexanderjaramillo <john.jara@gmail.com>
2017-10-05 15:00:59 -0500

    Authentication

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -8,13 +8,15 @@ const repository = 'psl-workshop-api-testing-js';
 
 describe('Github Api Test', () => {
   describe('Authentication', () => {
-    it('Via OAuth2 Tokens by Header', () =>
+    it('Via OAuth2 Tokens by Header', () => {
+      console.log(`Este es el token : ${process.env.ACCESS_TOKEN}`);
       agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .auth('token', process.env.ACCESS_TOKEN)
         .then((response) => {
           expect(response.status).to.equal(statusCode.OK);
           expect(response.body.description).equal('Repositorio para practica de NodeJS');
-        }));
+        });
+    });
     it('Via OAuth2 Tokens by parameter', () =>
       agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
         .query(`access_token=${process.env.ACCESS_TOKEN}`)
